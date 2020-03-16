3b6aa5ef9cae906a45b05aa50db7089101dfb400
Maja Barwińska <majakempa1@gmail.com>
2020-03-16 18:43:20 +0100

    initial commit

---
+++ src/main/java/pl/barwinscy/Akbarapp/AkbarAppApplication.java
@@ -0,0 +1,13 @@
+package pl.barwinscy.Akbarapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbarAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AkbarAppApplication.class, args);
+	}
+
+}
