acde9434ce4a0961997061b88732d76492fbf28e
Maja Barwińska <majakempa1@gmail.com>
2020-03-28 16:30:52 +0100

    created first controller and index page

---
+++ src/main/java/pl/barwinscy/Akbarapp/controllers/HomeController.java
@@ -0,0 +1,17 @@
+package pl.barwinscy.Akbarapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
+public class HomeController {
+
+
+    @GetMapping
+    public String getIndexPage(){
+        return "index";
+    }
+
+}
