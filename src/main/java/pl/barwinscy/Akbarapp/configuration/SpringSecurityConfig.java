7a4b00fdfa996975c05f9186094a6406c792dcef
Maja Barwińska <majakempa1@gmail.com>
2020-04-01 09:54:46 +0200

    added basic security config for development purposes

---
+++ src/main/java/pl/barwinscy/Akbarapp/configuration/SpringSecurityConfig.java
@@ -0,0 +1,24 @@
+package pl.barwinscy.Akbarapp.configuration;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        PasswordEncoder encoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password(encoder.encode("123"))
+                .roles("ADMIN");
+    }
+
+}
+
+
