d4b8c1f734d9043d6c69ccda4c75d7337bb00c32
Maja Barwińska <majakempa1@gmail.com>
2020-04-21 10:52:06 +0200

    added calendar view

---
+++ src/main/java/pl/barwinscy/Akbarapp/controllers/CalendarController.java
@@ -0,0 +1,13 @@
+package pl.barwinscy.Akbarapp.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class CalendarController {
+
+    @GetMapping("/calendar")
+    public String getCalendarPage(){
+        return "calendar";
+    }
+}
